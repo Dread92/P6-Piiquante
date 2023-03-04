630571d1dd03f3d37a21c123b0a6049bfe05eb86
BLANC_GUILLAUME <guillaume.blanc92@hotmail.fr>
2023-03-04 22:57:21 +0200

    installing cors and creating first routes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
-console.log("hello")
 const express = require("express")
-const server  = express()
+const server  = express() // on assigne le server à express
+const corsMiddleWare = require("cors") // on créé la variable pour utiliser cors
 const port = 3000
+
+
+server.use(corsMiddleWare()) // on utilise un .use pour enable cors
+
+// routes
+
 
 server.get('/', (req,res ) => res.send('hello'))
 
-server.listen(port, () => console.log("listening on port"+ port))
+server.listen(port, () => console.log("listening on port"+ port)) // notre serveur écoute sur le port 3000
