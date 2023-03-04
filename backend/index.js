f7e1c58e11b068811b387c5529a77b63dda48456
BLANC_GUILLAUME <guillaume.blanc92@hotmail.fr>
2023-03-04 23:50:28 +0200

    mongoose install

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,23 @@ const corsMiddleWare = require("cors") // on créé la variable pour utiliser co
 const port = 3000
 
 
-server.use(corsMiddleWare()) // on utilise un .use pour enable cors
+//database
+const  mongoose = require("mongoose");
+const password= "fLydAbrSLs3LUasw"
+const uri = ` mongodb+srv://guillaumeblanc92:${password}@cluster0.wuy64ly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority` 
 
+mongoose
+.connect(uri)
+.then(()=> console.log("connected to mongoose"))
+.catch (err => console.error("Error connecting to Mongo:", err))
+
+server.use(corsMiddleWare()) // on utilise un .use pour enable cors
+server.use(express.json())
 // routes
 
-
+server.post ("/api/auth/login", (req,res)=>{
+    console.log("login request", req.body)
+    res.send({message:" Utilisateur enregistré !"})
+} )
 server.get('/', (req,res ) => res.send('hello'))
-
 server.listen(port, () => console.log("listening on port"+ port)) // notre serveur écoute sur le port 3000
