1cd86ce3ce2e7bd25d2cfdf9c56ed683c3e657c4
BLANC_GUILLAUME <guillaume.blanc92@hotmail.fr>
2023-03-04 22:45:04 +0200

    installing express and enabling server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,8 @@
 console.log("hello")
+const express = require("express")
+const server  = express()
+const port = 3000
+
+server.get('/', (req,res ) => res.send('hello'))
+
+server.listen(port, () => console.log("listening on port"+ port))
