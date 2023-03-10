131e7a9248f76440fbd8e0e4eab45bad8af5f208
BLANC_GUILLAUME <guillaume.blanc92@hotmail.fr>
2023-03-10 03:29:07 +0200

    création du dossier models/sauces.js , schema

---
+++ backend/models/sauces.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose'); // on importe mongoose pour créer notre schéma
+
+// on créé un schéma de données pour les sauces avec ".Schema"
+const saucesSchema = mongoose.Schema({
+  title: { type: String, required: true },// en respectant notre const sauce= du fichier app.js, on ajoute les différentes clés requises( format type:string , required true)
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  userId: { type: String, required: true },
+  price: { type: Number, required: true }, // seul exception pour le prix qui sera type:number
+});
+
+// on exporte mongoose.model avec en argument "Sauces" et saucesSchema de notre fichier sauces.js
+module.exports = mongoose.model('Sauces', saucesSchema);
