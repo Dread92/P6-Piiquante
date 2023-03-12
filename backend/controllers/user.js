80dae5638e5f72a2d40ce2b0fb300b59619ebd70
BLANC_GUILLAUME <guillaume.blanc92@hotmail.fr>
2023-03-12 09:31:05 +0200

    login auth created

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -16,5 +16,22 @@ exports.signup = (req, res, next) => { // on créé une fonction signup pour l'e
 };
 
 exports.login = (req, res, next) => {// et la fonction login pour la connexion des utilisateurs existants
-
+    user.findOne({ email: req.body.email })// on utilise la méthode findOne pour filtrer juste l'adresse mail transmise par l'input
+    .then(user => {// si la méthode est réussie...
+        if (!user) {// ... on vérifie d'abord si elle est nulle , ce qui signifierai que l'utilisateur n'existe pas
+            return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});// ... donc on renvoi une erreur sans préciser sa nature, qui pourrait déjà constituer une fuite de données
+        }
+        bcrypt.compare(req.body.password, user.password) // si l'utilisateur existe, on utilise la méthode compare de bcrypt en passant d'abord l'input de l'utilisateur, puis le password de notre DB
+            .then(valid => {
+                if (!valid) {// si le match n'est pas bon, on retourne une erreur, floue également pour les mêmes raisons que !user
+                    return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                }
+                res.status(200).json({// si le compare est bon, on retourne un 200 OK
+                    userId: user._id,// l'objet contenant le user._id
+                    token: 'TOKEN' // puis un Token d'authentification
+                });
+            })
+            .catch(error => res.status(500).json({ error })); // si erreur, code 500 serveur
+    })
+    .catch(error => res.status(500).json({ error }));// si erreur, code 500 serveur
 };
