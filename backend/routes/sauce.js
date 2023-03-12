54588d4bb87c0e9cee409e2a3e3d22133c8d4007
BLANC_GUILLAUME <guillaume.blanc92@hotmail.fr>
2023-03-12 10:23:26 +0200

    importing multer

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,5 +1,5 @@
 const express = require('express');// on requiert express pour notre router
-
+const multer = require('../middleware/multer-config'); // on importe notre middleware multer 
 const router = express.Router();// on utilise la méthode router avant d'importer nos routes "app.x " depuis le fichier app.js
 const auth= require('auth'); // on requiert notre auth qui vérifie l'authenticité de nos tokens, puis nous l'appliquons à chaque gestionnaire de route du fichier.
 // on appelle auth en premier sur nos gestionnaires car la vérification doit passer AVANT que la route ne soit empruntée
@@ -11,7 +11,7 @@ const sauceController = require('../controllers/sauce.js');
 router.get('/',auth, sauceController.getAllStuff);
 
  // on ajoute une méthode post pour envoyer la réponse
- router.post('/', );
+ router.post('/', auth, multer,sauceController.createSauces );
 
 // on utilise la méthode get pour retrieve les requêtes get
 router.get('/', auth,sauceController.getAllSauce);
@@ -21,7 +21,7 @@ router.get('/', auth,sauceController.getAllSauce);
   router.get('/:id',auth,sauceController.getOneSauce );  
 
 // on rajoute une route "put" vers api/sauces avec l'id en paramètre.
-router.put('/:id',auth,sauceController.modifySauce);
+router.put('/:id',auth,multer,sauceController.modifySauce);
  
 // on rajoute une route "delete" pour la suppression d'une sauce.. 
 router.delete('/:id',auth,sauceController.deleteSauce );
